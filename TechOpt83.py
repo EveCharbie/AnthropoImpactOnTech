fbf1f725159cb4e8983a7a85cf15ad6e64895d02
Francisco Pascoa <francisco.pascoa@umontreal.ca>
2022-05-12 14:37:57 -0400

    Convergence non testée
    juste un checkpoint de l'avancée avec les phases

--- TechOpt83.py
+++ TechOpt83.py
@@ -54,61 +54,62 @@ def prepare_ocp(
     The OptimalControlProgram ready to be solved
     """
 
-    biorbd_model = biorbd.Model(biorbd_model_path)
+    biorbd_model = ( biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path) )
 
     # Add objective functions
     objective_functions = ObjectiveList()
     # objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_MARKERS, marker_index=1, weight=-1)
     objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", node=Node.ALL_SHOOTING, weight=100, phase=0)  # pk Node.ALL_SHOOTING?
-    #objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", node=Node.ALL_SHOOTING, weight=100, phase=1)  # phases
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", node=Node.ALL_SHOOTING, weight=100, phase=1)  # phases
 
-    # objective_functions.add(  # oui? non? je sais pas  phases
-    #     minimize_difference,
-    #     custom_type=ObjectiveFcn.Mayer,
-    #     node=Node.TRANSITION,
-    #     weight=100,
-    #     phase=1,
-    #     quadratic=True,
-    # )
+    objective_functions.add(  # oui? non? je sais pas  phases
+        minimize_difference,
+        custom_type=ObjectiveFcn.Mayer,
+        node=Node.TRANSITION,
+        weight=100,
+        phase=1,
+        quadratic=True,
+    )
 
-    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, min_bound=0.9, max_bound=1.1, weight=0.01)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, min_bound=.0, max_bound=.7, weight=.01, phase=0)
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_TIME, min_bound=.0, max_bound=.7, weight=.01, phase=1)
 
     # Dynamics
     dynamics = DynamicsList()
     dynamics.add(DynamicsFcn.TORQUE_DRIVEN)
-    #dynamics.add(DynamicsFcn.TORQUE_DRIVEN) # phases
-
-    # Constraints
-    # constraints = ConstraintList()  # phases
-    # constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.START, first_marker="m0", second_marker="m1", phase=0)
-    # constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m2", phase=0)
-    # constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="m0", second_marker="m1", phase=1)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN) # phases
 
     # Define control path constraint
     dof_mappings = BiMappingList()
-    dof_mappings.add("tau", to_second=[None, None, None, None, None, None, 0, 1, 2, 3, 4, 5], to_first=[6, 7, 8, 9, 10, 11])
+    dof_mappings.add("tau", to_second=[None, None, None, None, None, None, 0, 1, 2, 3, 4, 5], to_first=[6, 7, 8, 9, 10, 11], phase=0)
+    dof_mappings.add("tau", to_second=[None, None, None, None, None, None, 0, 1, 2, 3, 4, 5], to_first=[6, 7, 8, 9, 10, 11], phase=1)
 
-    nb_q = biorbd_model.nbQ()
-    nb_qdot = biorbd_model.nbQdot()
+    nb_q = biorbd_model[0].nbQ()
+    nb_qdot = biorbd_model[0].nbQdot()
     n_tau = nb_q - 6
 
     tau_min, tau_max, tau_init = -500, 500, 0
     u_bounds = BoundsList()
     u_bounds.add([tau_min] * n_tau, [tau_max] * n_tau)
+    u_bounds.add([tau_min] * n_tau, [tau_max] * n_tau)
 
     # Initial guesses
-    x = np.vstack((np.random.random((nb_q, 2)), np.random.random((nb_qdot, 2))))  # pourquoi 2 colonnes. rep: debut fin
+    x = np.vstack((np.random.random((nb_q, 2)), np.random.random((nb_qdot, 2))))
     x_init = InitialGuessList()
+    x_init.add(x, interpolation=InterpolationType.LINEAR)
     x_init.add(x, interpolation=InterpolationType.LINEAR)
 
     u_init = InitialGuessList()
     u_init.add([tau_init] * n_tau)
+    u_init.add([tau_init] * n_tau)
 
     # Path constraint
     x_bounds = BoundsList()
-    x_bounds.add(bounds=QAndQDotBounds(biorbd_model))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
 
-    # Contraintes de position
+
+    # Contraintes de position: PHASE 0 la montee vers le carpe
 
     # deplacement
     x_bounds[0].min[:3, :] = -.1
@@ -121,24 +122,18 @@ def prepare_ocp(
     # le salto autour de x
     x_bounds[0].min[3, 0] = 0
     x_bounds[0].max[3, 0] = 0
-    x_bounds[0].min[3, 1] = -2 * 3.14 - .1
-    x_bounds[0].max[3, 1] = 0
-    x_bounds[0].min[3, 2] = -2 * 3.14 - .1  # fin un tour vers l'avant
-    x_bounds[0].max[3, 2] = -2 * 3.14 + .1
+    x_bounds[0].min[3, 1:] = -2 * 3.14 - .1
+    x_bounds[0].max[3, 1:] = 0
     # limitation du tilt autour de y
     x_bounds[0].min[4, 0] = 0
     x_bounds[0].max[4, 0] = 0
-    x_bounds[0].min[4, 1] = - 3.14 / 4
-    x_bounds[0].max[4, 1] = 3.14 / 4
-    x_bounds[0].min[4, 2] = - 3.14 / 8
-    x_bounds[0].max[4, 2] = 3.14 / 8
+    x_bounds[0].min[4, 1:] = - 3.14 / 4
+    x_bounds[0].max[4, 1:] = 3.14 / 4
     # la vrille autour de z
     x_bounds[0].min[5, 0] = 0
     x_bounds[0].max[5, 0] = 0
-    x_bounds[0].min[5, 1] = 0
-    x_bounds[0].max[5, 1] = 3 * 3.14 + .1
-    x_bounds[0].min[5, 2] = 3 * 3.14 - .1  # fin vrille et demi
-    x_bounds[0].max[5, 2] = 3 * 3.14 + .1                            ################## ca d'l'air qu'il est capable!
+    x_bounds[0].min[5, 1:] = 0
+    x_bounds[0].max[5, 1:] = 3 * 3.14 + .1
 
     # bras droit t=0
     x_bounds[0].min[7, 0] = -2.9  # debut bras aux oreilles
@@ -154,19 +149,66 @@ def prepare_ocp(
     # le carpe
     x_bounds[0].min[10, 0] = 0
     x_bounds[0].max[10, 0] = 0
+    x_bounds[0].min[10, 2] = 2.8
+    x_bounds[0].max[10, 2] = 3.
     # le dehanchement
     x_bounds[0].min[11, 0] = 0
     x_bounds[0].max[11, 0] = 0
 
-    # Contraintes de vitesse
-    vzinit = 9.81 / 2 * final_time  # vitesse initiale en z du CoM pour revenir a terre au temps final
+    # Contraintes de position: PHASE 1 l'ouverture et la vrille et demie
+
+    # deplacement
+    x_bounds[1].min[:3, :] = -.1
+    x_bounds[1].max[:3, :] = .1
+    x_bounds[1].min[2, :] = 0
+    x_bounds[1].max[2, :] = 20  # beaucoup plus que necessaire, juste pour que la parabole fonctionne
+
+    # le salto autour de x
+    x_bounds[1].min[3, :] = -2 * 3.14 - .1
+    x_bounds[1].max[3, :] = 0
+    x_bounds[1].min[3, 2] = -2 * 3.14 - .1  # fin un tour vers l'avant
+    x_bounds[1].max[3, 2] = -2 * 3.14 + .1
+    # limitation du tilt autour de y
+    x_bounds[1].min[4, :] = - 3.14 / 4
+    x_bounds[1].max[4, :] = 3.14 / 4
+    x_bounds[1].min[4, 2] = - 3.14 / 8
+    x_bounds[1].max[4, 2] = 3.14 / 8
+    # la vrille autour de z
+    x_bounds[1].min[5, :] = 0
+    x_bounds[1].max[5, :] = 3 * 3.14 + .1
+    x_bounds[1].min[5, 2] = 3 * 3.14 - .1  # fin vrille et demi
+    x_bounds[1].max[5, 2] = 3 * 3.14 + .1                            ##################
+
+    # bras droit t=0
+    x_bounds[1].min[7, 2] = -2.9 - .1  # fini bras aux oreilles
+    x_bounds[1].max[7, 2] = -2.9 + .1
+    x_bounds[1].min[6, 2] = -.1
+    x_bounds[1].max[6, 2] = .1
+    # bras gauche t=0
+    x_bounds[1].min[9, 2] = 2.9 - .1  # fini bras aux oreilles
+    x_bounds[1].max[9, 2] = 2.9 + .1
+    x_bounds[1].min[8, 2] = -.1
+    x_bounds[1].max[8, 2] = .1
+
+    # le carpe
+    x_bounds[1].min[10, 0] = 2.8
+    x_bounds[1].max[10, 0] = 3.
+    x_bounds[1].min[10, 2] = -.1  # fini hanches ouvertes
+    x_bounds[1].max[10, 2] = .1
+    # le dehanchement
+    x_bounds[1].min[11, 2] = -.1
+    x_bounds[1].max[11, 2] = .1
+
+    # Contraintes de vitesse: PHASE 0 la montee vers le carpe
+
+    vzinit = 9.81 / 2 #* final_time  # vitesse initiale en z du CoM pour revenir a terre au temps final
     vrotxinit = -2 * 3.14  # vitesse initiale en rot x du CoM. 2pi pour un salto
 
     # decalage entre le bassin et le CoM
     CoM_Q_sym = MX.sym('CoM', nb_q)
     CoM_Q_init = np.zeros(nb_q)
-    CoM_Q_func = Function('CoM_Q_func', [CoM_Q_sym], [biorbd_model.CoM(CoM_Q_sym).to_mx()])
-    bassin_Q_func = Function('bassin_Q_func', [CoM_Q_sym], [biorbd_model.globalJCS(0).to_mx()])  # retourne la RT du bassin
+    CoM_Q_func = Function('CoM_Q_func', [CoM_Q_sym], [biorbd_model[0].CoM(CoM_Q_sym).to_mx()])
+    bassin_Q_func = Function('bassin_Q_func', [CoM_Q_sym], [biorbd_model[0].globalJCS(0).to_mx()])  # retourne la RT du bassin
 
     v = np.array(CoM_Q_func(CoM_Q_init)).reshape(1, 3) - np.array(bassin_Q_func(CoM_Q_init))[-1, :3]  # selectionne seulement la translation
 
@@ -184,8 +226,8 @@ def prepare_ocp(
     # autour de x
     x_bounds[0].min[15, :] = -100
     x_bounds[0].max[15, :] = 100
-    x_bounds[0].min[15, 0] = vrotxinit - .5
-    x_bounds[0].max[15, 0] = vrotxinit + .5
+    x_bounds[0].min[15, 0] = vrotxinit - 3.
+    x_bounds[0].max[15, 0] = vrotxinit + 3.
     # autour de y
     x_bounds[0].min[16, :] = -100
     x_bounds[0].max[16, :] = 100
@@ -198,6 +240,7 @@ def prepare_ocp(
     x_bounds[0].max[17, 0] = 0
 
     # tenir compte du decalage entre bassin et CoM avec la rotation
+    # Qtransdot = Qtransdot + v cross Qrotdot
     x_bounds[0].min[12:15, 0] = x_bounds[0].min[12:15, 0] + np.cross(v, x_bounds[0].min[15:18, 0])
     x_bounds[0].max[12:15, 0] = x_bounds[0].max[12:15, 0] + np.cross(v, x_bounds[0].max[15:18, 0])
 
@@ -218,8 +261,46 @@ def prepare_ocp(
     x_bounds[0].min[23, 0] = 0
     x_bounds[0].max[23, 0] = 0
 
-    # constraints = ConstraintList()
-    # constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.9, max_bound=1.1, phase=0)
+    # Contraintes de vitesse: PHASE 1 l'ouverture et la vrille et demie
+
+    # en xy bassin
+    x_bounds[1].min[12:14, :] = -10
+    x_bounds[1].max[12:14, :] = 10
+    # z bassin
+    x_bounds[1].min[14, :] = -100
+    x_bounds[1].max[14, :] = 100
+
+    # autour de x
+    x_bounds[1].min[15, :] = -100
+    x_bounds[1].max[15, :] = 100
+    # x_bounds[1].min[15, 2] = vrotxinit - 3.  # peut-être trop contraignant
+    # x_bounds[1].max[15, 2] = vrotxinit + 3.
+    # autour de y
+    x_bounds[1].min[16, :] = -100
+    x_bounds[1].max[16, :] = 100
+    # autour de z
+    x_bounds[1].min[17, :] = -100
+    x_bounds[1].max[17, :] = 100
+    # x_bounds[1].min[17, 2] = -10  # peut-être trop contraignant
+    # x_bounds[1].max[17, 2] = 10
+
+    # des bras
+    x_bounds[1].min[18:22, :] = -100
+    x_bounds[1].max[18:22, :] = 100
+    x_bounds[1].min[18:22, 2] = -10  # peut-être trop contraignant
+    x_bounds[1].max[18:22, 2] = 10
+
+    # du carpe
+    x_bounds[1].min[22, :] = -100
+    x_bounds[1].max[22, :] = 100
+    # x_bounds[1].min[22, 0] = -10  # peut-être trop contraignant
+    # x_bounds[1].max[22, 0] = 10
+    # du dehanchement
+    x_bounds[1].min[23, :] = -100
+    x_bounds[1].max[23, :] = 100
+    # x_bounds[1].min[23, 0] = -10  # peut-être trop contraignant
+    # x_bounds[1].max[23, 0] = 10
+
 
     return OptimalControlProgram(
         biorbd_model,
@@ -243,7 +324,7 @@ def main():
     Prepares and solves an ocp that has quaternion in it. Animates the results
     """
 
-    ocp = prepare_ocp("Models/JeCh_TechOpt83.bioMod", n_shooting=100, final_time=1.)
+    ocp = prepare_ocp("Models/JeCh_TechOpt83.bioMod", n_shooting=(50, 50), final_time=(.5, .5))
     ocp.add_plot_penalty(CostType.ALL)
     ocp.print(to_graph=True)
     solver = Solver.IPOPT(show_online_optim=True, show_options=dict(show_bounds=True))
@@ -254,8 +335,11 @@ def main():
 
     temps = time.strftime("%Y-%m-%d-%H%M%S")
     nom = 'sol'
-    qs = sol.states['q']
-    qdots = sol.states['qdot']
+    qs = sol.states[0]['q']
+    qdots = sol.states[0]['qdot']
+    for i in range(1, len(sol.states)):
+        qs = np.hstack((qs, sol.states[i]['q']))
+        qdots = np.hstack((qdots, sol.states[i]['qdot']))
     if True:  # switch manuelle
         np.save(f'Solutions/{nom}-{temps}-q.npy', qs)
         np.save(f'Solutions/{nom}-{temps}-qdot.npy', qdots)
