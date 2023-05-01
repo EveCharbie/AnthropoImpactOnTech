c372cb9ff169c54bfd495b1cdf1cff9b40c4f2f5
Charbie <eve.charbonneau.1@umontreal.ca>
2023-05-01 14:44:38 -0400

    added all joints trajectory length

--- graph_length_path_vs_passive_rotations.py
+++ graph_length_path_vs_passive_rotations.py
@@ -1,14 +1,18 @@
 
-import biorbd
-import bioviz
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import statsmodels.api as sm
 import pandas as pd
-import bioviz
 import os
 from IPython import embed
+import scipy
+
+import biorbd
+import bioviz
+import bioviz
+
 
 
 def compute_normalized_trajectory_length(model, q, marker_name):
@@ -104,9 +108,9 @@ for name in athletes_number.keys():
 
 
     index_arms = np.where(excel[:, 0] == name + " bras gauche bas, droit descend")[0][0]
-    arms_twist_potential = abs(excel[index_arms, 4])
+    arms_twist_potential = abs(excel[index_arms, 4]) * 360
     index_hips = np.where(excel[:, 0] == name + " bras en  bas, jambes tilt")[0][0]
-    hips_twist_potential = abs(excel[index_hips, 4])
+    hips_twist_potential = abs(excel[index_hips, 4]) * 360
 
     best_cost = np.min(cost)
     noise_index_to_keep = np.where(cost <= 1.05*best_cost)[0]
@@ -147,11 +151,11 @@ axs[1].set_xlabel("Arm twist potential [$^\circ$]")
 axs[1].set_ylabel("Normalized left arm trajectory length [m]")
 axs[2].set_xlabel("Hips twist potential [$^\circ$]")
 axs[2].set_ylabel("Normalized legs trajectory length [m]")
-axs[0].set_xlim(0.45, 0.8)
+axs[0].set_xlim(0.45*360, 0.8*360)
 axs[0].set_ylim(3.8, 10.15)
-axs[1].set_xlim(0.45, 0.8)
+axs[1].set_xlim(0.45*360, 0.8*360)
 axs[1].set_ylim(5.9, 10.6)
-axs[2].set_xlim(0.27, 0.42)
+axs[2].set_xlim(0.27*360, 0.42*360)
 axs[2].set_ylim(6.1, 8.1)
 plt.show()
 # plt.savefig(save_path + "clusters_length_path_for_all_athletes.png", dpi=300)
@@ -200,11 +204,11 @@ for i in range(6):
     # axs[i, 1].set_ylabel("Normalized left arm trajectory length [m]")
     # axs[i, 2].set_xlabel("Hips twist potential [$^\circ$]")
     # axs[i, 2].set_ylabel("Normalized legs trajectory length [m]")
-    axs[i, 0].set_xlim(0.45, 0.8)
+    axs[i, 0].set_xlim(0.45*360, 0.8*360)
     axs[i, 0].set_ylim(3.8, 10.15)
-    axs[i, 1].set_xlim(0.45, 0.8)
+    axs[i, 1].set_xlim(0.45*360, 0.8*360)
     axs[i, 1].set_ylim(5.9, 10.6)
-    axs[i, 2].set_xlim(0.27, 0.42)
+    axs[i, 2].set_xlim(0.27*360, 0.42*360)
     axs[i, 2].set_ylim(6.1, 8.1)
 
 axs[0, 0].set_title("Right Arm")
@@ -213,4 +217,50 @@ axs[0, 2].set_title("Hips")
 
 # plt.show()
 plt.savefig(save_path + "length_path_for_all_athletes_per_clusters.png", dpi=300)
+
+
+
+embed()
+def plot_length_path_for_all_solutions_all_joints(data_to_graph):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    i_athlete = 0
+    i_trajectory = 0
+    twist_potential = []
+    joints_trajectories = []
+    for name in athletes_number.keys():
+        color = cmap(i_athlete / 18)
+        for i, idx in enumerate(data_to_graph[name]["noise_index_to_keep"]):
+            twist_potential += [2*data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]]
+            joints_trajectories += [data_to_graph[name]["right_arm_trajectory"][idx] + data_to_graph[name]["left_arm_trajectory"][idx] + data_to_graph[name]["legs_trajectory"][idx]]
+            ax.plot(twist_potential[i_trajectory], joints_trajectories[i_trajectory], 'o', color=color)
+            i_trajectory += 1
+        i_athlete += 1
+        plt.plot(0, 0, 'o', color=color, label="Athlete #" + str(athletes_number[name]))
+
+    fig.subplots_adjust(right=0.8)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Athlete number
+    ax.set_xlabel("Twist potential [$^\circ$]")  # 2*Arm + Hips
+    ax.set_ylabel("Normalized summed joint trajectories length [m]")
+    ax.set_xlim(1.235*360, 2*360)
+    ax.set_ylim(15.45, 29.15)
+
+    return twist_potential, joints_trajectories
+
+twist_potential, joints_trajectories = plot_length_path_for_all_solutions_all_joints(data_to_graph)
+# plt.show()
+plt.savefig(save_path + "length_path_for_all_solutions_all_joints.png", dpi=300)
+
+_, _ = plot_length_path_for_all_solutions_all_joints(data_to_graph)
+correlation = scipy.stats.spearmanr(np.array(twist_potential), np.array(joints_trajectories))[0]
+lin_regress = scipy.stats.linregress(np.array(twist_potential), np.array(joints_trajectories))
+slope = lin_regress.slope
+intercept = lin_regress.intercept
+
+x_lin_regress = np.linspace(1.235*360, 2*360, 10)
+y_lin_regress = slope*x_lin_regress + intercept
+plt.plot(x_lin_regress, y_lin_regress, '-k', linewidth=0.5)
+plt.text(2*360-25, 29.15-0.5, "S=" + str(round(correlation, 2)), fontsize=10)
+plt.savefig(save_path + "length_path_for_all_solutions_all_joints_with_correlation.png", dpi=300)
+plt.show()
+
 
