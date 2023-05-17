8a8b8109db6fdb5f6b5e935b2a936b35a700d187
Charbie_OCP1 <eve.charbonneau.1@umontreal.ca>
2023-05-17 10:21:37 -0400

    updated animate_multimodel

--- animate_MultiModel.py
+++ animate_MultiModel.py
@@ -3,28 +3,31 @@ This code allows to animate de optimal solutions from the multi-model OCP.
 '''
 
 import bioviz
+import biorbd
 import pickle
 import numpy as np
+from bioptim import BiMappingList
 
-sol = "/home/mickaelbegon/Documents/Stage_Lisa/Anthropo Lisa/new_sol_double_vrille/AdCh_double_vrille_et_demi_0_CVG.pkl"
+sol = "/home/mickaelbegon/Documents/Eve/AnthropoImpactOnTech/Solutions_MultiModel/AdCh_AlAd_CVG.pkl"
 
 
 def animate(file, models, PKL_FLAG):
-    model_path = 'Models/Models_Lisa/AdCh.bioMod'
+    model_path = 'Models/AuJo_JeCh.bioMod'
     if PKL_FLAG:
         sol_file = open(file, "rb")
         dict_sol = pickle.load(sol_file)
-        # q_mapped = dict_sol['q_mapped'][0]
         # sol = dict_sol['sol']
         q = dict_sol['q']
-        # mapping = dict_sol['mapping']['to_second']
+        q_to_first = dict_sol['mapping']['to_first']
+        q_to_second = dict_sol['mapping']['to_second']
+
+        mappings = BiMappingList()
+        mappings.add("q", to_first=q_to_first, to_second=q_to_second)
+
+        q_mapped = mappings['q'].to_second.map(q)
 
         b = bioviz.Viz(model_path)
-        # q_mapped = []
-        # for i in range(len(mapping)):
-        #     q_mapped.append(q[mapping[i],:])
-        # q_mapped = np.array(q_mapped)
-        b.load_movement(q)
+        b.load_movement(q_mapped)
         b.exec()
 
     # q = np.load(sol)
