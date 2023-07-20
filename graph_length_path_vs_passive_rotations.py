28889ce0f5fe9d3e970611c21ca8efb94edb105e
Charbie <eve.charbonneau.1@umontreal.ca>
2023-07-20 17:47:27 +0200

    plotted 2.5 twists

--- graph_length_path_vs_passive_rotations.py
+++ graph_length_path_vs_passive_rotations.py
@@ -25,16 +25,10 @@ def compute_normalized_trajectory_length(model, q, marker_name):
     return np.sum(norm_marker_diff)
 
 
-nb_twists = 2
+nb_twists = 1
 save_path = "overview_graphs/"
 models_path = "Models/Models_Lisa/"
-
-if nb_twists == 1:
-    saved_data_path = "kinematics_graphs/vrille_et_demi/data_pickled/"
-elif nb_twists == 2:
-    saved_data_path = "kinematics_graphs/double_vrille_et_demi/data_pickled/"
-else:
-    raise ValueError("Number of twists not implemented")
+saved_data_path = "kinematics_graphs/vrille_et_demi/data_pickled/"
 
 cmap = cm.get_cmap('viridis')
 athletes_number = {"WeEm": 1,
@@ -71,7 +65,8 @@ with open(save_path + "clusters_sol.pkl", "rb") as f:
     cluster_left_arm = data["cluster_left_arm"]
     cluster_thighs = data["cluster_thighs"]
 
-    excel = pd.read_excel("degrees_of_liberty.xlsx", index_col=None, header=None).to_numpy()
+    path_to_degree_of_liberty = "Passive_rotations/passive rotations results/"
+    excel = pd.read_excel(f"{path_to_degree_of_liberty}degrees_of_liberty.xlsx", index_col=None, header=None).to_numpy()
 
 data_to_graph = {name: {"noise_idx": None, "noise_index_to_keep": None, "arms_twist_potential": None, "hips_twist_potential":None, "right_arm_trajectory": None, "left_arm_trajectory": None, "legs_trajectory": None} for name in athletes_number.keys()}
 markers = ['o', 'x', '^', '*', 's', 'p']
@@ -150,9 +145,9 @@ axs[1].set_xlabel("Arm twist potential [$^\circ$]")
 axs[1].set_ylabel("Normalized left arm trajectory length [m]")
 axs[2].set_xlabel("Hips twist potential [$^\circ$]")
 axs[2].set_ylabel("Normalized legs trajectory length [m]")
-axs[0].set_xlim(0.45*360, 0.8*360)
+axs[0].set_xlim(425, 775)
 axs[0].set_ylim(3.8, 10.15)
-axs[1].set_xlim(0.45*360, 0.8*360)
+axs[1].set_xlim(425, 775)
 axs[1].set_ylim(3.8, 10.15)
 axs[2].set_xlim(0.27*360, 0.42*360)
 axs[2].set_ylim(3.8, 10.15)
@@ -203,12 +198,12 @@ for i in range(6):
     # axs[i, 1].set_ylabel("Normalized left arm trajectory length [m]")
     # axs[i, 2].set_xlabel("Hips twist potential [$^\circ$]")
     # axs[i, 2].set_ylabel("Normalized legs trajectory length [m]")
-    axs[i, 0].set_xlim(0.45*360, 0.8*360)
-    axs[i, 0].set_ylim(3.8, 10.15)
-    axs[i, 1].set_xlim(0.45*360, 0.8*360)
-    axs[i, 1].set_ylim(5.9, 10.6)
-    axs[i, 2].set_xlim(0.27*360, 0.42*360)
-    axs[i, 2].set_ylim(6.1, 8.1)
+    axs[i, 0].set_xlim(425, 775)
+    axs[i, 0].set_ylim(3, 10.5)
+    axs[i, 1].set_xlim(425, 775)
+    axs[i, 1].set_ylim(3, 10.5)
+    # axs[i, 2].set_xlim(0.27*360, 0.42*360)
+    # axs[i, 2].set_ylim(6.1, 8.1)
 
 axs[0, 0].set_title("Right Arm")
 axs[0, 1].set_title("Left Arm")
@@ -229,38 +224,41 @@ def plot_length_path_for_all_solutions_all_joints(data_to_graph, graph_type="arm
         color = cmap(i_athlete / 18)
         for i, idx in enumerate(data_to_graph[name]["noise_index_to_keep"]):
             if graph_type == "arm_arm_hips":
-                twist_potential += [2*data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]]
-                joints_trajectories += [data_to_graph[name]["right_arm_trajectory"][idx] + data_to_graph[name]["left_arm_trajectory"][idx] + data_to_graph[name]["legs_trajectory"][idx]]
+                twist_potential += [data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]]
+                joints_trajectories += [(data_to_graph[name]["right_arm_trajectory"][idx] + data_to_graph[name]["left_arm_trajectory"][idx])/2 + data_to_graph[name]["legs_trajectory"][idx]]
             elif graph_type == "arm_arm":
-                twist_potential += [2*data_to_graph[name]["arms_twist_potential"]]
-                joints_trajectories += [data_to_graph[name]["right_arm_trajectory"][idx] + data_to_graph[name]["left_arm_trajectory"][idx]]
+                twist_potential += [data_to_graph[name]["arms_twist_potential"]]
+                joints_trajectories += [(data_to_graph[name]["right_arm_trajectory"][idx] + data_to_graph[name]["left_arm_trajectory"][idx])/2]
             else:
                 raise ValueError("graph_type must be either 'arm_arm_hips' or 'arm_arm'")
 
             ax.plot(twist_potential[i_trajectory], joints_trajectories[i_trajectory], 'o', color=color)
             i_trajectory += 1
         if graph_type == "arm_arm_hips":
-            twist_potential_per_athlete[name] = 2*data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]
+            twist_potential_per_athlete[name] = data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]
         else:
-            twist_potential_per_athlete[name] = 2*data_to_graph[name]["arms_twist_potential"]
+            twist_potential_per_athlete[name] = data_to_graph[name]["arms_twist_potential"]
         i_athlete += 1
         plt.plot(0, 0, 'o', color=color, label="Athlete #" + str(athletes_number[name]))
 
     fig.subplots_adjust(right=0.8)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Athlete number
-    if graph_type == "arm_arm_hips":   # 2*Arm + Hips
-        ax.set_xlabel(r"2 $\times$ arm + hips twist potential [$^\circ$]")
+    if graph_type == "arm_arm_hips":   # Arm + Hips
+        ax.set_xlabel(r"arm + hips twist potential [$^\circ$]")
         ax.set_ylabel("Normalized combined arms and hips trajectories length [m]")
     else:
-        ax.set_xlabel(r"2 $\times$ arm twist potential [$^\circ$]")
+        ax.set_xlabel(r"arm twist potential [$^\circ$]")
         ax.set_ylabel("Normalized combined arms trajectories length [m]")
     if graph_type == "arm_arm_hips":
-        ax.set_xlim(435, 725)
-        ax.set_ylim(15.45, 29.15)
+        # ax.set_xlim(435, 725)
+        # ax.set_ylim(15.45, 29.15)
+        ax.set_xlim(525, 900)
+        ax.set_ylim(10, 20)
         print('ici')
     else:
-        ax.set_xlim(335, 590)
-        ax.set_ylim(9, 21)
+        print('ici')
+        ax.set_xlim(400, 800)
+        ax.set_ylim(4, 11)
 
     return twist_potential, twist_potential_per_athlete, joints_trajectories
 
@@ -278,10 +276,10 @@ lin_regress = scipy.stats.linregress(np.array(twist_potential), np.array(joints_
 slope = lin_regress.slope
 intercept = lin_regress.intercept
 
-x_lin_regress = np.linspace(435, 725, 10)
+x_lin_regress = np.linspace(500, 900, 10)
 y_lin_regress = slope*x_lin_regress + intercept
 plt.plot(x_lin_regress, y_lin_regress, '-k', linewidth=0.5)
-plt.text(2*360-20, 29.15-0.5, "S=" + str(round(correlation, 2)), fontsize=10)
+plt.text(860, 19.5, "S=" + str(round(correlation, 2)), fontsize=10)
 plt.savefig(save_path + "length_path_for_all_solutions_all_joints_with_correlation.png", dpi=300)
 plt.show()
 
@@ -355,3 +353,54 @@ cbar = fig.colorbar(color_bar_handle, cax=cbar_ax)
 cbar.ax.set_title('Combined\ntwist potential [$\circ$]')
 plt.savefig("overview_graphs/musculature_height_twist_potential.png", dpi=300)
 # plt.show()
+
+
+# 2.5 Twists -----------------------------------------------------------------------------------------------------------
+nb_twists = 2
+
+results_path = 'solutions_multi_start/'
+results_path_this_time = results_path + f'Solutions_double_vrille_et_demi/'
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+for i_name, name in enumerate(athletes_number.keys()):
+
+    color = cmap(i_name / 18)
+    model = biorbd.Model(models_path + name + ".bioMod")
+    full_twist_potential = data_to_graph[name]["arms_twist_potential"] + data_to_graph[name]["hips_twist_potential"]
+
+    for i_sol in range(9):
+        file_name = results_path_this_time + name + '/' + name + f'_double_vrille_et_demi_' + str(i_sol) + "_CVG.pkl"
+        if not os.path.isfile(file_name):
+            continue
+
+        with open(file_name, 'rb') as f:
+            data = pickle.load(f)
+        Q = data['q']
+
+        q_joined = np.zeros(np.shape(Q[0][:, :-1]))
+        q_joined[:, :] = Q[0][:, :-1]
+        for i in range(1, len(Q)):
+            if i == len(Q) - 1:
+                q_joined = np.hstack((q_joined, Q[i]))
+            else:
+                q_joined = np.hstack((q_joined, Q[i][:, :-1]))
+
+        q_joined[:6, :] = 0
+        length_right_arm_trajectory = compute_normalized_trajectory_length(model, q_joined, "RightArmNormalized")
+        length_left_arm_trajectory = compute_normalized_trajectory_length(model, q_joined, "LeftArmNormalized")
+        length_leg_trajectory = compute_normalized_trajectory_length(model, q_joined, "LegsNormalized")
+        full_trajectory = (length_right_arm_trajectory + length_left_arm_trajectory)/2 + length_leg_trajectory
+
+        ax.plot(full_twist_potential, full_trajectory, 'o', color=color)
+
+        if i_sol == 0:
+            plt.plot(0, 0, 'o', color=color, label="Athlete #" + str(athletes_number[name]))
+
+fig.subplots_adjust(right=0.8)
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Athlete number
+ax.set_xlim(650, 900)
+ax.set_ylim(12, 23)
+ax.set_xlabel(r"arm + hips twist potential [$^\circ$]")
+ax.set_ylabel("Normalized combined arms and hips trajectories length [m]")
+plt.savefig(save_path + "length_path_for_all_solutions_all_joints_double_vrille.png", dpi=300)
+plt.show()
